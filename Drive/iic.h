f9ee79b6c6bd4d09a9de5af6bf86d6f8e75f23d9
Zipeng.Feng <614842291@qq.com>
2022-09-03 17:51:48 +0800

    [Driver][IIC]Move define to C file
    [Report]
    Not Test
    Not Build

--- Drive/iic.h
+++ Drive/iic.h
@@ -2,9 +2,6 @@
 #define __I2C_H
 #include "stm32f10x.h"
 #include "systick.h"
-#define IIC_GPIO GPIOB
-#define IIC_SCL GPIO_Pin_10
-#define IIC_SDA GPIO_Pin_11
 
 typedef enum
 {
@@ -13,21 +10,6 @@ typedef enum
     E_IIC_ERROR,
     E_IIC_MAX
 }E_IIC_Status_TypeDef;
-
-void SDA_OUT(void);
-void SDA_IN(void);
-//IO操作函数	
-#define IIC_SCL_H     GPIO_SetBits(IIC_GPIO,IIC_SCL)//SCL
-#define IIC_SCL_L     GPIO_ResetBits(IIC_GPIO,IIC_SCL)//SCL
-
-#define IIC_SDA_H     GPIO_SetBits(IIC_GPIO,IIC_SDA)//SDA
-#define IIC_SDA_L     GPIO_ResetBits(IIC_GPIO,IIC_SDA)//SDA
-
-#define READ_SDA      GPIO_ReadInputDataBit(IIC_GPIO,IIC_SDA)  //输入SDA 
-
-  //IIC所有操作函数
-void IIC_Init(void);                //初始化IIC的IO口
-
 
 
 unsigned char I2C_Readkey(unsigned char I2C_Addr);
@@ -46,6 +28,8 @@ E_IIC_Status_TypeDef  Api_IIC_Read_OneByte(u8 daddr,u8 addr,u8* data);
 
 E_IIC_Status_TypeDef Api_IIC_WriteBytes(u8 DeviceID,u8 RegAddr,u8 size,u8* pBuff);
 E_IIC_Status_TypeDef Api_IIC_ReadBytes(u8 DeviveID,u8 RegAddr,u8 size,u8* pBuff);
+void Api_IIC_Init(void);
+
 
 
 #endif
